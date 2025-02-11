df1ca46602a4aa6bc050c5d5309837e3395de3cc
Dinesh Bobburu <bobburudinesh123@gmail.com>
2025-02-11 14:43:24 -0600

    Initial Commit

---
+++ Debug/ThirdParty/FreeRTOS/croutine.d
@@ -0,0 +1,21 @@
+ThirdParty/FreeRTOS/croutine.o: ../ThirdParty/FreeRTOS/croutine.c \
+ D:/Embedded_Systems/Projects/STM32_Projects/FreeRTOS_STM32/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ D:/Embedded_Systems/Projects/STM32_Projects/FreeRTOS_STM32/ThirdParty/FreeRTOS/FreeRTOSConfig.h \
+ D:/Embedded_Systems/Projects/STM32_Projects/FreeRTOS_STM32/ThirdParty/FreeRTOS/include/projdefs.h \
+ D:/Embedded_Systems/Projects/STM32_Projects/FreeRTOS_STM32/ThirdParty/FreeRTOS/include/portable.h \
+ D:/Embedded_Systems/Projects/STM32_Projects/FreeRTOS_STM32/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ D:/Embedded_Systems/Projects/STM32_Projects/FreeRTOS_STM32/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ D:/Embedded_Systems/Projects/STM32_Projects/FreeRTOS_STM32/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ D:/Embedded_Systems/Projects/STM32_Projects/FreeRTOS_STM32/ThirdParty/FreeRTOS/include/task.h \
+ D:/Embedded_Systems/Projects/STM32_Projects/FreeRTOS_STM32/ThirdParty/FreeRTOS/include/list.h \
+ D:/Embedded_Systems/Projects/STM32_Projects/FreeRTOS_STM32/ThirdParty/FreeRTOS/include/croutine.h
+D:/Embedded_Systems/Projects/STM32_Projects/FreeRTOS_STM32/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+D:/Embedded_Systems/Projects/STM32_Projects/FreeRTOS_STM32/ThirdParty/FreeRTOS/FreeRTOSConfig.h:
+D:/Embedded_Systems/Projects/STM32_Projects/FreeRTOS_STM32/ThirdParty/FreeRTOS/include/projdefs.h:
+D:/Embedded_Systems/Projects/STM32_Projects/FreeRTOS_STM32/ThirdParty/FreeRTOS/include/portable.h:
+D:/Embedded_Systems/Projects/STM32_Projects/FreeRTOS_STM32/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+D:/Embedded_Systems/Projects/STM32_Projects/FreeRTOS_STM32/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+D:/Embedded_Systems/Projects/STM32_Projects/FreeRTOS_STM32/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+D:/Embedded_Systems/Projects/STM32_Projects/FreeRTOS_STM32/ThirdParty/FreeRTOS/include/task.h:
+D:/Embedded_Systems/Projects/STM32_Projects/FreeRTOS_STM32/ThirdParty/FreeRTOS/include/list.h:
+D:/Embedded_Systems/Projects/STM32_Projects/FreeRTOS_STM32/ThirdParty/FreeRTOS/include/croutine.h:
---
+++ Debug/ThirdParty/FreeRTOS/timers.d
@@ -0,0 +1,25 @@
+ThirdParty/FreeRTOS/timers.o: ../ThirdParty/FreeRTOS/timers.c \
+ D:/Embedded_Systems/Projects/STM32_Projects/FreeRTOS_STM32/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ D:/Embedded_Systems/Projects/STM32_Projects/FreeRTOS_STM32/ThirdParty/FreeRTOS/FreeRTOSConfig.h \
+ D:/Embedded_Systems/Projects/STM32_Projects/FreeRTOS_STM32/ThirdParty/FreeRTOS/include/projdefs.h \
+ D:/Embedded_Systems/Projects/STM32_Projects/FreeRTOS_STM32/ThirdParty/FreeRTOS/include/portable.h \
+ D:/Embedded_Systems/Projects/STM32_Projects/FreeRTOS_STM32/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ D:/Embedded_Systems/Projects/STM32_Projects/FreeRTOS_STM32/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ D:/Embedded_Systems/Projects/STM32_Projects/FreeRTOS_STM32/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ D:/Embedded_Systems/Projects/STM32_Projects/FreeRTOS_STM32/ThirdParty/FreeRTOS/include/task.h \
+ D:/Embedded_Systems/Projects/STM32_Projects/FreeRTOS_STM32/ThirdParty/FreeRTOS/include/list.h \
+ D:/Embedded_Systems/Projects/STM32_Projects/FreeRTOS_STM32/ThirdParty/FreeRTOS/include/queue.h \
+ D:/Embedded_Systems/Projects/STM32_Projects/FreeRTOS_STM32/ThirdParty/FreeRTOS/include/task.h \
+ D:/Embedded_Systems/Projects/STM32_Projects/FreeRTOS_STM32/ThirdParty/FreeRTOS/include/timers.h
+D:/Embedded_Systems/Projects/STM32_Projects/FreeRTOS_STM32/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+D:/Embedded_Systems/Projects/STM32_Projects/FreeRTOS_STM32/ThirdParty/FreeRTOS/FreeRTOSConfig.h:
+D:/Embedded_Systems/Projects/STM32_Projects/FreeRTOS_STM32/ThirdParty/FreeRTOS/include/projdefs.h:
+D:/Embedded_Systems/Projects/STM32_Projects/FreeRTOS_STM32/ThirdParty/FreeRTOS/include/portable.h:
+D:/Embedded_Systems/Projects/STM32_Projects/FreeRTOS_STM32/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+D:/Embedded_Systems/Projects/STM32_Projects/FreeRTOS_STM32/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+D:/Embedded_Systems/Projects/STM32_Projects/FreeRTOS_STM32/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+D:/Embedded_Systems/Projects/STM32_Projects/FreeRTOS_STM32/ThirdParty/FreeRTOS/include/task.h:
+D:/Embedded_Systems/Projects/STM32_Projects/FreeRTOS_STM32/ThirdParty/FreeRTOS/include/list.h:
+D:/Embedded_Systems/Projects/STM32_Projects/FreeRTOS_STM32/ThirdParty/FreeRTOS/include/queue.h:
+D:/Embedded_Systems/Projects/STM32_Projects/FreeRTOS_STM32/ThirdParty/FreeRTOS/include/task.h:
+D:/Embedded_Systems/Projects/STM32_Projects/FreeRTOS_STM32/ThirdParty/FreeRTOS/include/timers.h:
